6c7b13268e8082419b09dde25251b931cd7d473d
TymurRozm <105302850+TymurRozm@users.noreply.github.com>
2024-06-09 13:18:05 +0300

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,72 @@
+variable "vpc_id" {
+  type = string
+}
+
+terraform { 
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region = "eu-north-1"
+}
+
+resource "aws_security_group" "docker_sg" {
+  name_prefix = "docker_sg"
+  description = "Security group for Docker containers"
+  vpc_id      = var.vpc_id
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 2375
+    to_port     = 2375
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "docker_instance" {
+  ami                    = "ami-064087b8d355e9051"
+  instance_type          = "t3.micro"
+  key_name               = "lab"
+  vpc_security_group_ids = [aws_security_group.docker_sg.id]
+
+  user_data = <<-EOF
+    #!/bin/bash
+    sudo apt-get update
+    sudo apt-get install -y nginx
+    sudo systemctl start nginx
+    sudo systemctl enable nginx
+    echo "<h3>Brigada2</h3><h3>Terraform1</h3>" > /var/www/html/index.html
+  EOF
+
+  tags = {
+    Name = "Docker Instance"
+  }
+}
